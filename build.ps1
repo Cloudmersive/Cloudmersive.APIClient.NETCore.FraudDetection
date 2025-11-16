a1ec3f8595e1ef3ecdffd036a51cbeda139b9d01
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2025-11-16 01:00:04 +0000

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,11 +1,11 @@
 ﻿Remove-Item –path ./client –recurse
 
 
-Invoke-WebRequest -Uri 'https://api.cloudmersive.com/cdr/docs/v1/swagger' -OutFile '.\cdr-api-swagger.json'
-(Get-Content .\cdr-api-swagger.json).replace('localhost', "api.cloudmersive.com") | Set-Content .\cdr-api-swagger.json
-(Get-Content .\cdr-api-swagger.json -Raw) -replace '"http"','"https"' | Set-Content .\cdr-api-swagger.json -Encoding UTF8
+Invoke-WebRequest -Uri 'https://api.cloudmersive.com/fraud-ai/docs/v1/swagger' -OutFile '.\fraud-api-swagger.json'
+(Get-Content .\fraud-api-swagger.json).replace('localhost', "api.cloudmersive.com") | Set-Content .\fraud-api-swagger.json
+(Get-Content .\fraud-api-swagger.json -Raw) -replace '"http"','"https"' | Set-Content .\fraud-api-swagger.json -Encoding UTF8
 
-& java -jar swagger-codegen-cli-2.4.12.jar generate -i .\cdr-api-swagger.json -l csharp -o client -c packageconfig.json
+& java -jar swagger-codegen-cli-2.4.12.jar generate -i .\fraud-api-swagger.json -l csharp -o client -c packageconfig.json
 #(Get-Content ./client/src/api/ConvertDocumentApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertDocumentApi.js
 #(Get-Content ./client/src/api/ConvertWebApi.js).replace('var returnType = Object;', "var returnType = 'Blob';") | Set-Content ./client/src/api/ConvertWebApi.js
 #& npm build ./client
